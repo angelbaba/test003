045961487578f0b32ec35ef3749ea539836adbb8
jin <jpy1600518154@outlook.com>
2020-08-04 14:01:28 +0800

    dev分支 提交

--- src/main/java/com/itcast/moticer/config/Myconfigration.java
+++ src/main/java/com/itcast/moticer/config/Myconfigration.java
@@ -6,7 +6,9 @@ import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class Myconfigration {
-
+    public static void main(String[] args) {
+        System.out.println("dev数据");
+    }
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
