1432b05be498c0d1c1ac7ccd26c98ed7acd20b08
jin <jpy1600518154@outlook.com>
2020-08-04 14:02:07 +0800

    mast分支 提交

--- src/main/java/com/itcast/moticer/config/Myconfigration.java
+++ src/main/java/com/itcast/moticer/config/Myconfigration.java
@@ -9,7 +9,7 @@ public class Myconfigration {
     ///金大爷
 
     public static void main(String[] args) {
-        System.out.println("11111111111");
+        System.out.println("1111111master分支1111");
     }
     @Bean
     public RestTemplate restTemplate() {
