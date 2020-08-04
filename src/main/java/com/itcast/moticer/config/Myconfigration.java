44a35c119c4ad5ad489d078553227f587a4b3130
jin <jpy1600518154@outlook.com>
2020-08-04 14:14:41 +0800

    合并分支 提交

--- src/main/java/com/itcast/moticer/config/Myconfigration.java
+++ src/main/java/com/itcast/moticer/config/Myconfigration.java
@@ -9,6 +9,7 @@ public class Myconfigration {
     public static void main(String[] args) {
         System.out.println("dev数据");
         System.out.println("1111111master分支1111");
+        System.out.println("合并完成后");
     }
     ///金大爷
 
