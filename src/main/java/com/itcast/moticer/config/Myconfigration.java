2a0d73abc177c7f939397de7ea262a327f7f4934
jin <jpy1600518154@outlook.com>
2020-08-04 13:53:03 +0800

    master分支 提交

--- src/main/java/com/itcast/moticer/config/Myconfigration.java
+++ src/main/java/com/itcast/moticer/config/Myconfigration.java
@@ -6,7 +6,11 @@ import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class Myconfigration {
+    ///金大爷
 
+    public static void main(String[] args) {
+        System.out.println("11111111111");
+    }
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
